cc36abb9fcbf074306e3eed371a7a67f577e7533
Michael Klier <chi@chimeric.de>
2009-01-14 08:22:14 +0100

    added support for new page dialog
    darcs-hash:20090114072214-23886-af518a81229d9f72330a7a081e2149301dbd4fca.gz

--- main.php
+++ main.php
@@ -76,9 +76,15 @@ require_once(DOKU_TPLINC.'tpl_functions.php');
 
       <ul id="top__nav">
         <?php
+			if(!plugin_isdisabled('npd') && ($npd =& plugin_load('helper', 'npd'))) {
+                $npb = $npd->html_new_page_button(true);
+                if($npb) {
+                    print '<li>' . $npb . '</li>' . DOKU_LF;
+                }
+            }
             foreach(array('edit', 'history', 'subscribe', 'subscribens') as $act) {
                 ob_start();
-                print '<li>' . DOKU_LF;
+                print '<li>';
                 if(tpl_actionlink($act)) {
                     print '</li>' . DOKU_LF;
                     ob_end_flush();
