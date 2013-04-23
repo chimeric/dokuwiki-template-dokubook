a3638d70008691842a4adc369ef2219d63d538f2
haobug <qingxianhao@gmail.com>
2013-04-23 23:33:50 +0800

    Chinese string translation

---
+++ lang/zh/settings.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * english language file for the configuration manager
+ *
+ * @author:     Michael Klier <chi@chimeric.de>
+ */
+
+$lang['sb_pagename'] = "用户导航的页面名字";
+$lang['sb_position'] = "侧边栏的位置";
+$lang['ft_pagename'] = "用户页脚的页面名字";
+$lang['closedwiki']  = "封闭的维基(如果没有登录导航只显示登录表单)";
+$lang['logo']        = "Logo 图片的路径";
